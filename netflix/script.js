f12615793aaa5196e0b8a6aa6477f6851bd9fae0
Pratik Anand <136880678+PratikAnand26@users.noreply.github.com>
2024-04-04 19:39:39 +0530

    Add files via upload

---
+++ netflix/script.js
@@ -0,0 +1,48 @@
+function openans1(){
+    document.getElementById("ansone").style.display="flex";
+    document.getElementById("iconone").style.display="none"
+    }
+function closeans1(){
+        document.getElementById("ansone").style.display="none";
+        document.getElementById("iconone").style.display="inline";
+    }
+function openans2(){
+        document.getElementById("anstwo").style.display="flex";
+        document.getElementById("icontwo").style.display="none"
+    }
+function closeans2(){
+        document.getElementById("anstwo").style.display="none";
+        document.getElementById("icontwo").style.display="inline";
+    }
+function openans3(){
+        document.getElementById("ansthree").style.display="flex";
+        document.getElementById("iconthree").style.display="none"
+}
+function closeans3(){
+        document.getElementById("ansthree").style.display="none";
+        document.getElementById("iconthree").style.display="inline";
+}
+function openans4(){
+    document.getElementById("ansfour").style.display="flex";
+    document.getElementById("iconfour").style.display="none"
+}
+function closeans4(){
+    document.getElementById("ansfour").style.display="none";
+    document.getElementById("iconfour").style.display="inline";
+}
+function openans5(){
+    document.getElementById("ansfive").style.display="flex";
+    document.getElementById("iconfive").style.display="none"
+}
+function closeans5(){
+    document.getElementById("ansfive").style.display="none";
+    document.getElementById("iconfive").style.display="inline";
+}
+function openans6(){
+    document.getElementById("anssix").style.display="flex";
+    document.getElementById("iconsix").style.display="none"
+}
+function closeans6(){
+    document.getElementById("anssix").style.display="none";
+    document.getElementById("iconsix").style.display="inline";
+}
